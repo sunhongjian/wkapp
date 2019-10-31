5cab9e0278361c7c699f3466ccd1fe9f9af23448
sunhongjian <2969948238@qq.com>
2019-10-31 15:36:28 +0800

    [~] 增加按两次返回

--- src/main.vue
+++ src/main.vue
@@ -2,17 +2,16 @@
   <!-- App -->
   <f7-app :params="f7params">
     <f7-statusbar></f7-statusbar>
-    <f7-view id="main-view" main url="/">
-      
-    </f7-view>
-        <!-- Login Screen -->
+    <f7-view id="main-view" main url="/"></f7-view>
+    <!-- Login Screen -->
     <login-page></login-page>
   </f7-app>
 </template>
 <script>
 // Import Routes...
 import routes from "./routes.js";
-import loginPage from './assets/vue/pages/login'
+import loginPage from "./assets/vue/pages/login";
+import global from './global';
 
 let theme = "auto";
 if (document.location.search.indexOf("theme=") >= 0) {
@@ -23,18 +22,15 @@ export default {
   components: {
     loginPage
   },
+  // 双击返回
   mounted() {
-    let self = this;
-    document.addEventListener(
-      "deviceready",
-      function() {
-        document.addEventListener("backbutton", exitApp, false);
-        function exitApp() {
-          navigator.app.exitApp();
-        }
-      },
-      false
-    );
+    document.removeEventListener("backbutton", this.onBackKeyDown, false);
+    document.addEventListener("backbutton", this.onBackKeyDown, false);
+  },
+  beforeDestroy() {
+    window.clearInterval(this.intervalID);
+    document.removeEventListener("backbutton", this.onBackKeyDown, false);
+    document.removeEventListener("backbutton", this.exitApp, false);
   },
   data() {
     return {
@@ -49,6 +45,23 @@ export default {
         }
       }
     };
+  },
+  methods: {
+    onBackKeyDown() {
+      global.toast("再按一次退出");
+      document.removeEventListener("backbutton", this.onBackKeyDown, false);
+      document.addEventListener("backbutton", this.exitApp, false);
+      this.intervalID = window.setInterval(() => {
+        document.removeEventListener("backbutton", this.exitApp, false);
+        document.addEventListener("backbutton", this.onBackKeyDown, false);
+        window.clearInterval(this.intervalID);
+      }, 3000);
+    },
+    exitApp() {
+      console.log(navigator);
+      console.log(navigator.app);
+      navigator.app.exitApp();
+    }
   }
 };
 </script>
