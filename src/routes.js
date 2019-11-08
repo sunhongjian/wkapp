e6313b018774daa139b4431aaf67cafac8ab17f3
sunhongjian <2969948238@qq.com>
2019-11-08 18:15:10 +0800

    [~] 删除多余文件

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,7 @@
 import Home from './assets/vue/pages/home.vue';
 import AboutPage from './assets/vue/pages/about.vue';
-import FormPage from './assets/vue/pages/form.vue';
-import DynamicRoutePage from './assets/vue/pages/dynamic-route.vue';
 
 import PanelLeftPage from './assets/vue/pages/panel-left.vue';
-import ColorThemes from './assets/vue/pages/color-themes.vue';
-import Chat from './assets/vue/pages/chat.vue';
 import Vuex from './assets/vue/pages/vuex.vue';
 
 import Index from './assets/vue/pages/index'
@@ -46,24 +42,8 @@ export default [
     component: Version
   },
   {
-    path: '/form/',
-    component: FormPage
-  },
-  {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    component: DynamicRoutePage
-  },
-  {
     path: '/panel-left/',
     component: PanelLeftPage
-  },
-  {
-    path: '/color-themes/',
-    component: ColorThemes
-  },
-  {
-    path: '/chat/',
-    component: Chat
   },
   {
     path: '/manage/',
